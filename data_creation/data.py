540623a1e49f610d3c65f3ef23134fcd2d0d06e5
Yannick Clausen <yannick.clausen@haw-hamburg.de>
2021-05-23 19:21:59 +0200

    Add parallelization and more logging

--- data_creation/data.py
+++ data_creation/data.py
@@ -6,30 +6,39 @@
 
 import csv
 import time
+import multiprocessing
+import os
 
 #
-# CONSTANTS
+# CONFIG
 #
 
 # Total search space, rectangle with (x1, y1, x2, y2)
-T = [1, 1, 6, 6]
+T = [1, 1, 100, 100]
 # Search space resolution, where smaller means finer
 # Values < 1 are not supported (yet)
 res = 1
 # Set of masses to test
 M = [1, 2, 3]
+OUT = './output/'
 # Chunk count
-chunk_count_per_dim = 3
+chunk_count_per_dim = 33
+# Estimate for one simulation run in seconds
+EST_SIM = 0.01
+
+#
+# CONSTANTS
+#
+
 chunk_count = chunk_count_per_dim ** 2
 # Size of a search space chunk with (a, b)
 C = [abs(T[2] - T[0] + 1) // chunk_count_per_dim,
      abs(T[3] - T[1] + 1) // chunk_count_per_dim]
 # Total number of samples
 N = C[1] * C[0] * len(M) * chunk_count // res
-# Number of samples per chunk, `abs(C.a) * abs(C.b) * len(M) / res`
+# Number of samples per chunk
 N_C = C[1] * C[0] * len(M) // res
 # Output folder name
-OUT = './output/'
 
 #
 # FUNCTIONS
@@ -39,11 +48,12 @@ OUT = './output/'
 # Stub for the sim function
 def sim(x, y, m):
     # Simulate longer runtimes
-    # time.sleep(2)
+    time.sleep(EST_SIM)
     return x * y * m
 
 
-# this will be 1 process later
+# Process one chunk identified by start and end values for x and y
+# M is the set of masses to test with
 def process(X, Y, M):
     L = []
     for x in range(X[0], X[1]):
@@ -56,41 +66,77 @@ def process(X, Y, M):
 #
 # SCRIPT
 #
-print('********************************************************************************')
-print('ASAI PLANETS - SIMULATION')
-print('********************************************************************************')
-print()
-print(f'Search space with coordinates: ({T[0]}, {T[1]}) ({T[2]}, {T[3]})')
-print(f'Search space size: {T[2] - T[0] + 1}x{T[3] - T[1] + 1}')
-print(f'Search space resolution: {res}')
-print(f'Total number of samples: {N}')
-print(f'Chunk count: {chunk_count}')
-print(f'Chunk size: {C[0]}x{C[1]}')
-print(f'Number of samples per chunk: {N_C}')
-print(f'Testing masses: {M}')
-print()
-print('********************************************************************************')
-print()
+if __name__ == '__main__':
+    time_per_chunk = N_C * EST_SIM
+    avg_chunks_per_core = chunk_count / os.cpu_count()
+    total_time = time_per_chunk * max(avg_chunks_per_core, 1)
+    print('********************************************************************************')
+    print('ASAI PLANETS - SIMULATION')
+    print('********************************************************************************')
+    print()
+    print(f'Search space with coordinates: ({T[0]}, {T[1]}) ({T[2]}, {T[3]})')
+    print(f'Search space size: {T[2] - T[0]}x{T[3] - T[1]}')
+    print(f'Search space resolution: {res}')
+    print(f'Total number of samples: {N}')
+    print(f'Chunk count: {chunk_count}')
+    print(f'Chunk size: {C[0]}x{C[1]}x{len(M)}')
+    print(f'Number of samples per chunk: {N_C}')
+    print(f'Testing masses: {M}')
+    print()
+    print(f'Using {os.cpu_count()} CPUs.')
+    print(f'Average chunks per core: {avg_chunks_per_core}')
+    print(f'Estimated time per chunk: {time_per_chunk}s')
+    print()
+    print(f'Estimated total time: {total_time}s')
+    print()
+    print('********************************************************************************')
+    print()
 
-L = []
-for y in range(chunk_count_per_dim):
-    for x in range(chunk_count_per_dim):
-        print(f'Calculating chunk ({x + y * chunk_count_per_dim + 1} of {chunk_count})')
-        T = [1, 1, 6, 6]
-        X = [T[0] + x * C[0],
-             T[0] + (x + 1) * C[0]]
-        Y = [T[1] + y * C[1],
-             T[1] + (y + 1) * C[1]]
-        L = L + process(X, Y, M)
+    ###########################################################################
+    # Create data
 
-file_name = f'{OUT}{T[0]}_{T[1]}-{T[2]}_{T[3]}.csv'
-with open(file_name, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',',
-                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for row in L:
-        writer.writerow(row)
+    print('Calculating chunks...')
+    start_time = time.time()
+    args = []
 
-print()
-print('********************************************************************************')
-print(f'Wrote results to {file_name}')
-print('********************************************************************************')
+    for y in range(chunk_count_per_dim):
+        for x in range(chunk_count_per_dim):
+            X = [T[0] + x * C[0],
+                 T[0] + (x + 1) * C[0]]
+            Y = [T[1] + y * C[1],
+                 T[1] + (y + 1) * C[1]]
+            args.append([X, Y, M])
+
+    result = []
+    with multiprocessing.Pool() as p:
+        r = p.starmap_async(process, args)
+        result = r.get()
+
+    # Print some info about how long things took
+    finish_time = time.time()
+    elapsed = finish_time - start_time
+    avg_time_per_chunk = elapsed / avg_chunks_per_core
+    avg_time_per_sim = avg_time_per_chunk / N_C
+    total_time = time_per_chunk * max(avg_chunks_per_core, 1)
+
+    print('Done\n')
+    print(f'Avg. time per chunk: {avg_time_per_chunk}s')
+    print(f'Avg. time per sim: {avg_time_per_sim}s')
+    print(f'Finished in {elapsed}s')
+
+    ###########################################################################
+    # Write data
+
+    file_name = f'{OUT}{T[0]}_{T[1]}-{T[2]}_{T[3]}.csv'
+
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for chunk in result:
+            for row in chunk:
+                writer.writerow(row)
+
+    print()
+    print('********************************************************************************')
+    print(f'Wrote results to {file_name}')
+    print('********************************************************************************')
