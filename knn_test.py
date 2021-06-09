4ba5bf35a9b9c36861c843c74d3a065473e3d5db
hphaw <44225863+hphaw@users.noreply.github.com>
2021-06-09 16:22:38 +0200

    knn is now predicting values to produce 'upscaled' image
    also changed process of saving the image from pygame to PIL -> way faster and more efficient

--- knn_test.py
+++ knn_test.py
@@ -1,14 +1,16 @@
-import os
 import csv
+from math import sqrt
+import os
 
 import pandas as pd
 import numpy as np
 
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.utils import shuffle
+from PIL import Image
 
-# Amount of data used for training the knn
-training_percent = 80
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+
 
 # Number of neighbors (tests resulted in 5 having the lowest error)
 neighbors = 5
@@ -16,36 +18,53 @@ neighbors = 5
 columns = ['x', 'y', 'm', 'score']
 
 
+render_scale = 4
+
+renders_path = os.path.join(os.path.abspath(os.getcwd()), 'renders')
+
 # reading data
 data_path = os.path.join(os.path.abspath(os.getcwd()), 'output')
 data = pd.read_csv(os.path.join(data_path, 'X[-1.5, 1.5]__Y[1.5, -1.5]__M[3e+27]__N[1442401].csv'), names=columns, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
+# extracting used parameters
+T = [data['x'].min(), data['x'].max(), data['y'].max(), data['y'].min()]
+M = data['m'].unique()
+N = data.shape[0]
+resolution_per_axis = int( sqrt(N / len(M)) * render_scale )
+
+# constructing new data with better reolution
+X = np.linspace(T[0], T[2], resolution_per_axis)
+Y = np.linspace(T[1], T[3], resolution_per_axis)
+args = []
+
+
 # removing mass from everything if only one is present
-if len(data['m'].unique()) == 1:
+if len(M) == 1:
     columns.remove('m')
     data.drop(columns=['m'], inplace=True)
+    args = [[x, y] for y in Y for x in X]
+else:
+    args = [[x, y, m] for m in M for y in Y for x in X]
 
-# Shuffling of the data so that the division into training and test data does not distort the result
-data = shuffle(data).reset_index(drop=True)
+del X, Y
 
 # Splitting into training and test data
-train = data[:int(len(data)*(training_percent/100))].reset_index(drop=True)
-test = data[int(len(data)*(training_percent/100)):].reset_index(drop=True)
+X = data[columns[:-1]].to_numpy()
+y = data[columns[-1]].to_numpy()
 
-X_train = train[columns[:-1]]
-y_train = train[columns[-1]]
-
-X_test = test[columns[:-1]]
-y_test = test[columns[-1]]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33)
 
 knn = KNeighborsRegressor(n_neighbors=neighbors, weights='distance', n_jobs=-1)
 knn.fit(X_train, y_train)
 
-test_predicted = X_test.join(pd.DataFrame(knn.predict(X_test), columns=['pred_score'])).join(y_test)
-test_predicted['error'] = test_predicted.apply(lambda row: abs(row['pred_score'] - row['score']), axis=1)
+y_pred = knn.predict(X_test)
+print('Mean squared error: ' + str(mean_squared_error(y_test,y_pred)))
 
-error = test_predicted['error'].mean()
 
-print(test_predicted)
+pred_data = np.array(knn.predict(args))
 
-print(error)
+pred_data.resize((resolution_per_axis, resolution_per_axis))
+im = Image.fromarray(np.uint8(np.interp(pred_data, [0, 1], [0, 255])))
+im.save(os.path.join(renders_path, f'N{N}_R{render_scale}.png'))
+
+print('Exported predicted image to: ' + os.path.join(renders_path, f'N{N}_R{render_scale}.png'))
--- output_data_vis.py
+++ output_data_vis.py
@@ -1,80 +1,32 @@
 import os
-import sys
-from math import sqrt, log
-import colorsys
+from math import sqrt
 
-import pygame
 import csv
 
 import pandas as pd
-from numpy import interp
+import numpy as np
+
+from PIL import Image
 
 
-class Visualization():
-    def __init__(self, filename, selected_mass=2, zoom=5):
-
-        self.zoom = zoom
-        self.running = True
-
-        self.data_path = os.path.join(os.path.abspath(os.getcwd()), 'output')
-        self.data = pd.read_csv(os.path.join(self.data_path, filename), names=['x', 'y', 'm', 'score'], delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-        mass = list(sorted(set(self.data['m'])))[selected_mass]
-        print(mass)
-
-        self.data = self.data[self.data['m'] == mass]
-
-        self.width = int(sqrt(self.data.shape[0])) * self.zoom
-        self.height = self.width
-
-        print(self.width, self.height)
-
-    def handle_event(self, event):
-        if event.type == pygame.QUIT:
-            self.running = False
-            pygame.quit()
-            sys.exit()
-
-    def draw(self):
-        # render frame
-        self.window.fill((0, 0, 0))
-
-        for i in range(self.data.shape[0]):
-
-            # linear scale
-            score = self.data.iat[i, 3]
-
-            # logarithmic scale
-            # score = log((self.data.iat[i, 3]+1), 2)
-
-            # Grey color spectrum
-            c = [interp(score, [0, 1], [0, 255]) for _ in range(3)]
-
-            # HSV color spectrum
-            # c = [interp(cc, [0,1], [0,255]) for cc in colorsys.hsv_to_rgb(score, 0.6, 1)]
-
-            x = (i * self.zoom) % self.width
-            y = (i * self.zoom) // self.height * self.zoom
-
-            pygame.draw.rect(self.window, c, pygame.Rect(x, y, self.zoom, self.zoom))
-
-        pygame.display.flip()
-
-    def start(self):
-        pygame.init()
-
-        self.window = pygame.display.set_mode((self.width, self.height), 0)
-        pygame.display.set_caption('Data Visualization')
-
-        self.draw()
-        print('finished drawing')
-
-        while self.running:
-            # handle events
-            for event in pygame.event.get():
-                self.handle_event(event)
+filename = 'X[-1.5, 1.5]__Y[1.5, -1.5]__M[3e+27]__N[1442401].csv'
+selected_mass = 0
 
 
-if __name__ == '__main__':
-    v = Visualization(filename='X[-1.2, 1.2]__Y[1.2, -1.2]__M[1e+28, 1e+29, 1e+30]__N[30000].csv')
-    v.start()
+renders_path = os.path.join(os.path.abspath(os.getcwd()), 'renders')
+
+data_path = os.path.join(os.path.abspath(os.getcwd()), 'output')
+data = pd.read_csv(os.path.join(data_path, filename), names=['x', 'y', 'm', 'score'], delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+N = data.shape[0]
+resolution_per_axis = int(sqrt(N))
+
+mass = list(sorted(set(data['m'])))[selected_mass]
+data = data[data['m'] == mass]
+
+data.sort_values(by=['y', 'x'], ascending=[False, True], inplace=True)
+scores = np.array(data['score'])
+
+scores.resize((resolution_per_axis, resolution_per_axis))
+im = Image.fromarray(np.uint8(np.interp(scores, [0, 1], [0, 255])))
+im.save(os.path.join(renders_path, f'N{N}.png'))
